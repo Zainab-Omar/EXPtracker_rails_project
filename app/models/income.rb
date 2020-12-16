31af42f268c74011177371587ffc04a664b51b70
Zainab-Omar <zainab.alshaikhli01@gmail.com>
2020-12-15 22:41:40 -0600

    create signup and signin routes

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,4 +1,5 @@
 class Income < ApplicationRecord
+    belongs_to :user
     has_many :expenses
     has_many :categories, through: :expenses
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  post '/signout', to: 'sessions#destroy'
+  
   resources :incomes
   resources :categories
   resources :expenses
