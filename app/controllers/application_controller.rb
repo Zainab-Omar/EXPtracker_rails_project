166dad3cc43d22cebb76bf8c822725f4219fb534
Zainab-Omar <zainab.alshaikhli01@gmail.com>
2020-12-16 17:14:28 -0600

    create logged_in? and current_user methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
 
     def home
-        
+        if logged_in?
+            redirect_to user_path(current_user)
+        end
+    end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    
+    def logged_in?
+        !!current_user
     end
 end
