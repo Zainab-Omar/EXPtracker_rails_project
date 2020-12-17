e38e1b4a3099cc5d162a319cb7318b4564071160
Zainab-Omar <zainab.alshaikhli01@gmail.com>
2020-12-17 14:39:24 -0600

    build expense new form

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,5 +3,11 @@ class ExpensesController < ApplicationController
     def index
         @expenses = Expense.all
     end
+
+    def new
+        if logged_in?
+        @expense = Expense.new
+        end
+    end
   
 end
