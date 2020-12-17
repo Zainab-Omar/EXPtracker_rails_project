1b0867865a6d2d481a6334da49b801ad0a2c4cc7
Zainab-Omar <zainab.alshaikhli01@gmail.com>
2020-12-17 14:42:46 -0600

    create getter and setter method in expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,6 +2,15 @@ class Expense < ApplicationRecord
     belongs_to :category
     belongs_to :account
     belongs_to :user
+
+
+    def category_name=(name)
+        self.category = Category.find_or_create_by(name: name)
+    end
+    
+    def category_name
+        self.category ? self.category.name : nil
+    end
+
 end
 
-#bi-directional has_many through
